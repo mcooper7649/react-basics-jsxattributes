5ddefd057ee4b7f8ab445edde90433ce46952b56
Michael Cooper <55610025+mcooper7649@users.noreply.github.com>
2020-04-16 09:53:20 -0400

    css with react
    basics of react styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img1 =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcaptainaltcoin.com%2Fwp-content%2Fuploads%2F2018%2F08%2Fchainlink.jpg&f=1&nofb=1";
+const img2 =
+  "https://logos-download.com/wp-content/uploads/2016/06/Bitcoin_logo_yellow.png";
+const img3 =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1000logos.net%2Fwp-content%2Fuploads%2F2018%2F04%2FEthereum-Logo.png&f=1&nofb=1";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading">My favorite Coins</h1>
+    <div>
+      <img className="coin-img" src={img1} />
+      <img className="coin-img" src={img2} />
+      <img className="coin-img" src={img3} />
+    </div>
   </div>,
   document.getElementById("root")
 );
